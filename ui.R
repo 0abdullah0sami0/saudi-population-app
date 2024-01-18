9f57afcbaa7ee6be5b4b122d2c67c186b3c454da
0abdullah0sami0 <0abdullah0sami0@gmail.com>
2024-01-18 17:00:16 +0300

    Fix Logo

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ dashboardPage(
     tags$head(
       tags$link(rel = "shortcut icon", href = "logo.ico")
     ),
-    navbarPage(title=img(id="logo", src="Good logo.png", style="width:100%;height:150%"),
+    navbarPage(title=div(img(id="logo", src="Good logo.png", style="width:8%;height:10%; position:relative;display:right-align;margin-top:-10px;")),
                theme = shinytheme("lumen"),
                windowTitle = "Saudi Arabia Population",
                tabPanel("عربي",
